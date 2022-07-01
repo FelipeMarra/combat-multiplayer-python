b21115bc5ccf8b283587efb10c26f466f994d8f1
Felipe Marra <felipeferreiramarra@aluno.ufsj.edu.br>
2022-07-01 17:04:44 -0300

    better autorun

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-import os
 import socket
+import subprocess
+import threading
 
 LOCAL_IP = socket.gethostbyname(socket.gethostname())
 SERVER_PORT = 6666
@@ -7,13 +8,18 @@ TEST_MODE = "True"
 N_CLIENTS = 2
 
 def run_server():
-    os.popen(f"python -m app.server.server {SERVER_PORT}")
+    command = f"python -m app.server.server {SERVER_PORT}"
+    subprocess.call(command, creationflags=subprocess.CREATE_NEW_CONSOLE)
 
-def run_client():
-    os.popen(f"python -m app.client.client {LOCAL_IP} {SERVER_PORT} {TEST_MODE}")
+
+def run_client(cli_n):
+    command = f"python -m app.client.client {LOCAL_IP} {SERVER_PORT} {TEST_MODE}"
+    subprocess.call(command, creationflags=subprocess.CREATE_NEW_CONSOLE)
 
 if __name__ == "__main__":
-    run_server()
+    server_thread = threading.Thread(target=run_server)
+    server_thread.start()
 
     for n in range(N_CLIENTS):
-        run_client()
+        client_thread = threading.Thread(target=run_client, args=(n,))
+        client_thread.start()
