d4a1b9b75f15242cd04553e6aa6f41bb83e5fd99
Felipe Marra <felipeferreiramarra@aluno.ufsj.edu.br>
2022-07-01 14:35:04 -0300

    .

--- app/server/server.py
+++ app/server/server.py
@@ -6,14 +6,6 @@ import threading
 import pickle
 from tkinter import E
 
-from inspect import getsourcefile
-
-current_path = os.path.abspath(getsourcefile(lambda:0))
-current_dir = os.path.dirname(current_path)
-parent_dir = current_dir[:current_dir.rfind(os.path.sep)]
-
-sys.path.insert(0, parent_dir)
-
 from app import *
 
 class Server:
