db4707d562467e3d06d8ce7b53ff80c2c6ab3206
Felipe Marra <felipeferreiramarra@aluno.ufsj.edu.br>
2022-07-14 22:02:13 -0300

    .

--- app/server_models.py
+++ app/server_models.py
@@ -27,13 +27,3 @@ class BulletData():
 class GameData():
     def __init__(self, map):
         self.map = map
-
-#Eu me conecto ao servidor
-#peço meu Player Data
-    #eu crio meu objeto player local
-#eu fico esperando receber um novo player data que vai ser meu inimigo
-    #eu crio o objeto desse novo playe
-#a cada update meu
-    #envio meu Player Data e o server joga esse update pros demais clients
-#eu possuo um loop infinito que recebe updates de player data dos outros
-    #a cada update do pid inimigo, eu atualiso o Player Data correspondente
