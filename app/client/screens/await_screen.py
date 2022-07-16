ccf297ba299f1e196a5c179db7a9d6e8078b2e27
Hugo <58451345+hugogalli@users.noreply.github.com>
2022-07-16 15:09:11 -0300

    changing english issue
    some opponent -> for opponent

--- app/client/screens/await_screen.py
+++ app/client/screens/await_screen.py
@@ -28,7 +28,7 @@ def show(game):
             game.screen.blit(game.waiting_image, game.waiting_image_rect)
 
             if show_text:
-                screen.show_text(game, f'WAITING SOME OPPONENT', 32, PILLS[game.map - 1], WIDTH / 2, round(HEIGHT / 6))
+                screen.show_text(game, f'WAITING FOR OPPONENT', 32, PILLS[game.map - 1], WIDTH / 2, round(HEIGHT / 6))
 
             if game.state == TRADE_UPDATES_STATE:
                 waiting = False
